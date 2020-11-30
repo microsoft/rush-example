aee5b7c2b219d1d7583195e226ed181c3ed261ee
Gireesh Kumar <tas.service360.live@gmail.com>
2020-11-30 15:36:50 +0400

    testing change

--- tools/my-toolchain/src/start.ts
+++ tools/my-toolchain/src/start.ts
@@ -3,7 +3,7 @@
 import * as colors from 'colors';
 import * as child_process from 'child_process';
 
-console.log('Invoking my-toolchain...');
+console.log('Invoking 1-1-my-toolchain...');
 
 child_process.execSync('tsc', { stdio: 'inherit' });
 
