5fed8a14ab03b5096a89bfa8f9cfd4ab5106a8de
Diorbert Pereira <diorbert@gmail.com>
2023-03-20 14:43:27 -0400

    update config

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
     "allowPostUpgradeCommandTemplating": true,
     "allowedPostUpgradeCommands": [
-        "echo running rush update",
         "node common/scripts/install-run-rush.js update",
         "node common/scripts/install-run-rush.js change --bulk --message \"{{{prTitle}}}\" --bump-type patch"
     ]
